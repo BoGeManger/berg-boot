b2d7068d8dee5e3f8125b169097f339ea2c0e72b
libg <664120843@qq.com>
2020-10-14 14:05:52 +0800

    no message

--- common/src/main/java/com/berg/constant/AppConstants.java
+++ common/src/main/java/com/berg/constant/AppConstants.java
@@ -5,8 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-@Component
 @Data
+@Component
 public  class AppConstants {
 
     @Value("${spring.application.name}")
--- sql/db/table/sys/sys_file_downloadlog_tbl.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `sys_file_downloadlog_tbl` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '系统文件下载记录表id',
-  `path` text NOT NULL COMMENT '文件路径',
-  `download_user` varchar(64) NOT NULL COMMENT '创建人',
-  `download_timestamp` datetime NOT NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统文件下载记录表';
